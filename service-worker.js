0f6c8e06aec7acea5d47249f556a619e7119a192
roybert33-cmd <roybert33@gmail.com>
2026-07-06 23:09:41 -0400

    Subida inicial segura.

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'pizzas-roy-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './diseno.css',
+  './icono-pizza.png',
+  './AW.jpg',
+  './pizza.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
